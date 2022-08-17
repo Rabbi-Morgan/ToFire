f96eb4b0e769da6ea6059070cfe181b847549f97
Anuoluwapo <francismobolaji1990@gmail.com>
2022-08-18 00:07:57 +0100

    done with the first part of app

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -1,10 +1,10 @@
-import { List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core'
+import { List, ListItem, ListItemText } from '@material-ui/core'
 import React from 'react'
 
 function Todo({todo,ind}) {
   return <List>
     <ListItem>
-      <ListItemText primary={todo} secondary={`This is todo number ${ind+1}`}></ListItemText>
+      <ListItemText primary={todo} key={ind} secondary={`This is todo number ${ind+1}`}></ListItemText>
     </ListItem>
   </List>
 }
--- src/Component/Todos.js
+++ src/Component/Todos.js
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react"
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import { Button, FormControl, Input, InputLabel, Select, MenuItem, Grid, Container } from '@material-ui/core'
 import Todo from "./Todo";
 import './Styles/Todo.css'
 import { db } from "../firebase";
+import firebase from "firebase/compat/app";
 
 function Todos() {
   const [todos, setTodos] = useState([])
   const [input,setInput] = useState("");
+  const [order,setOrder] = useState("desc")
   const eventInput = (event)=>{
     setInput(event.target.value) 
+  }
+  const changeOrder = event => {
+    setOrder(event.target.value)
   }
   const addTodo = event =>{
 
     // setTodos([...todos, input])  //former way of adding the todo without our database
     db.collection('todos').add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       todo: input
     })
     event.preventDefault()
     setInput("")
   }
   useEffect(()=>{
-    db.collection("todos").onSnapshot(snapshot => {
+    db.collection("todos").orderBy('timestamp', order).onSnapshot(snapshot => {
       console.log(snapshot.docs.map(doc=>doc.data().todo))
       setTodos(snapshot.docs.map(doc=>doc.data().todo))
     })
@@ -30,8 +36,21 @@ function Todos() {
   // }, [input])
   return (
     <div className="App">
+      <Container>
       <form onSubmit={addTodo}>
         <h2>Todos for the great programmer Morgan</h2>
+        <Grid container spacing={3}>
+          <Grid itemn xs={6}>
+        <FormControl>
+          <InputLabel id="label">Order-by</InputLabel>
+          <Select onChange={changeOrder} labelId="label" id="select" value="desc">
+          <MenuItem value="desc">Descend by date</MenuItem>
+          <MenuItem value="acnd">Ascend by date</MenuItem>
+</Select>
+
+        </FormControl>
+        </Grid>
+        <Grid itemn xs={6}>
         <FormControl>
           <InputLabel htmlFor="todo">Todo Item</InputLabel>
          <Input onChange={eventInput} value={input} type='text' id="todo"/>
@@ -39,12 +58,15 @@ function Todos() {
         <FormControl>
         <Button disabled={!input} type="submit" variant="contained" color="primary">ADD TODO</Button>
         </FormControl>
+        </Grid>
+        </Grid>
         <ul>
           {todos.map((todo,ind)=> <Todo todo={todo} ind ={ind} key={ind}/>)}
         </ul>
         
         {/* <button type="submit">Submit</button> */}
       </form>
+      </Container>
     </div>
   );
 }
